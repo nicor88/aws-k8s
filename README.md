bc445eace2ed83cf1f11b9668fb9d4a59b210c89
nicor88 <nico@8fit.com>
2018-12-14 12:54:21 +0100

    init setup

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # aws-k8s
 Run kubernetes(k8s) inside AWS
+
+There are multiple ways to run Kubernetes on AWS, one of this based on using [EKS](https://aws.amazon.com/eks/).
+
+In this repository you can find multiple ways how deploy K8S based on EKS
+
+## Requirements
+* [awscli](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html)
+* [kubectl](https://docs.aws.amazon.com/eks/latest/userguide/configure-kubectl.html)
+* [heptio-authenticator-aws](https://docs.aws.amazon.com/eks/latest/userguide/configure-kubectl.html) (check the section **To install heptio-authenticator-aws for Amazon EKS**)
+
+
+## Deployment with Cloudformation
+
+1. Create the Master Node stack
+	<pre>
+	bash cloudformation/create_master_stack.sh
+	</pre>
+
+2. Create the Master Node stack
+	<pre>
+	bash cloudformation/create_master_stack.sh
+	</pre>
+
+3. Retrieve EKS Cluster information
+	<pre>
+	aws eks describe-cluster --name dev --query cluster.endpoint
+	aws eks describe-cluster --name dev --query cluster.certificateAuthority.data
+	</pre>
---
+++ cloudformation/setup_all.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+set -e
+
+if [ -z "$AWS_DEFAULT_PROFILE" ]; then
+    echo "Please set the AWS_DEFAULT_PROFILE environment variable"
+    exit 1
+fi
+
+# setup
+CLUSTER_NAME="dev-data"
+ARN_USER=$(aws sts get-caller-identity --output text --query 'Arn')
+IAM_USER=$(echo $ARN_USER | cut -d "/" -f2)
+
+echo "Creating the cluster from user $IAM_USER"
+
+# master
+MASTER_STACK_NAME="$CLUSTER_NAME-k8s-master"
+MASTER_TEMPLATE_URL="file://master.yml"
+#
+#echo 'Checking master template validity'
+#aws cloudformation validate-template --template-body $MASTER_TEMPLATE_URL
+#
+#echo 'Template valid, creating master stack'
+#aws cloudformation create-stack \
+#	--stack-name $MASTER_STACK_NAME \
+#	--template-body $MASTER_TEMPLATE_URL \
+#	--capabilities CAPABILITY_NAMED_IAM \
+#	--parameters ParameterKey=ClusterName,ParameterValue=$CLUSTER_NAME
+#
+#echo 'Waiting until master stack create completes'
+#aws cloudformation wait stack-create-complete --stack-name $MASTER_STACK_NAME
+#
+#echo '$MASTER_STACK_NAME created successfully'
+
+# nodes
+NODES_STACK_NAME="$CLUSTER_NAME-k8s-nodes"
+NODES_TEMPLATE_URL="file://nodes.yml"
+
+echo "Checking nodes template validity"
+aws cloudformation validate-template --template-body $NODES_TEMPLATE_URL
+
+echo "Template valid, creating nodes stack"
+
+aws cloudformation create-stack \
+	--stack-name $NODES_STACK_NAME \
+	--template-body $NODES_TEMPLATE_URL \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameters ParameterKey=ClusterName,ParameterValue=$CLUSTER_NAME
+
+echo "Waiting until nodes stack create completes"
+aws cloudformation wait stack-create-complete --stack-name $NODES_STACK_NAME
+
+echo "$NODES_STACK_NAME created successfully"
+
+EKS_ENDPOINT=$(aws eks describe-cluster --name $CLUSTER_NAME --query cluster.endpoint)
+EKS_CERTIFICATE_AUTH=$(aws eks describe-cluster --name $CLUSTER_NAME --query cluster.certificateAuthority.data)
+
+mkdir -p $HOME/.kube
+
+sed "s/EKS_CLUSTER/$CLUSTER_NAME/g" config/kube_config.dist.yml > config_tmp.yml
+sed "s,EKS_ENDPOINT,$EKS_ENDPOINT,g" config_tmp.yml > config_tmp2.yml
+sed "s/EKS_CERTIFICATION_AUTHORITY/$EKS_CERTIFICATE_AUTH/g" config_tmp2.yml > config_tmp3.yml
+
+cp config_tmp3.yml $HOME/.kube/config_$CLUSTER_NAME
+
+rm config_tmp.yml
+rm config_tmp2.yml
+rm config_tmp3.yml
+
+mkdir -p $HOME/.kube
+
+echo "Running: export KUBECONFIG=$HOME/.kube/config_$CLUSTER_NAME"
+export KUBECONFIG=$HOME/.kube/config_$CLUSTER_NAME
+
+#kubectl get nodes
+#
+## get IAM profile for nodes
+#
+#EC2_IAM_PROFILE=$(aws cloudformation describe-stacks --stack-name nico-dev-k8s-master --query 'Stacks[0].Outputs')
