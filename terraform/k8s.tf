b0cbe485af0ebd3c140d10462f105f8967b6acba
nicor88 <nicor88@users.noreply.github.com>
2019-05-17 09:57:35 +0200

    some little changes

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -10,59 +10,59 @@ module "network" {
   private_subnets    = "10.0.2.0/24,10.0.4.0/24,10.0.6.0/24"
 }
 
-module "eks-cluster" {
-  source = "./modules/eks-cluster"
+//module "eks-cluster" {
+//  source = "./modules/eks-cluster"
+//
+//  project = "${var.project}"
+//  stage   = "${var.stage}"
+//
+//  vpc_id = "${module.network.vpc_id}"
+//
+//  subnet_ids = "${concat(
+//    "${split(",", module.network.public_subnet_ids)}",
+//    "${split(",", module.network.private_subnet_ids)}"
+//    )}"
+//
+//  additional_security_groups = []
+//  additional_cidr_blocks     = ["0.0.0.0/0"]
+//
+//  k8s_version           = "1.12"
+//  enable_private_access = true
+//  enable_public_access  = true
+//}
 
-  project = "${var.project}"
-  stage   = "${var.stage}"
-
-  vpc_id = "${module.network.vpc_id}"
-
-  subnet_ids = "${concat(
-    "${split(",", module.network.public_subnet_ids)}",
-    "${split(",", module.network.private_subnet_ids)}"
-    )}"
-
-  additional_security_groups = []
-  additional_cidr_blocks     = ["0.0.0.0/0"]
-
-  k8s_version           = "1.12"
-  enable_private_access = true
-  enable_public_access  = true
-}
-
-module "eks-workers-default" {
-  source = "./modules/eks-workers"
-
-  project  = "${var.project}"
-  stage    = "${var.stage}"
-  asg_type = "default"
-
-  eks_cluster_name                       = "${module.eks-cluster.cluster_eks_name}"
-  eks_cluster_endpoint                   = "${module.eks-cluster.cluster_eks_endpoint}"
-  eks_cluster_certificate_authority_data = "${module.eks-cluster.cluster_certificate_authority_data}"
-  eks_cluster_security_group_id          = "${module.eks-cluster.cluster_eks_sg_id}"
-
-  min_size      = 1
-  max_size      = 3
-  instance_type = "t3.small"
-
-  # pick the right image from here: https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html
-  image_id = "ami-0abcb9f9190e867ab"
-
-  vpc_id     = "${module.network.vpc_id}"
-  subnet_ids = "${split(",", module.network.public_subnet_ids)}"
-}
-
-data "aws_iam_policy_document" "workers_default_iam_policy_extension" {
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:Get*"]
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "workers_default_iam_role_extension" {
-  policy = "${data.aws_iam_policy_document.workers_default_iam_policy_extension.json}"
-  role   = "${module.eks-workers-default.worker_role_id}"
-}
+//module "eks-workers-default" {
+//  source = "./modules/eks-workers"
+//
+//  project  = "${var.project}"
+//  stage    = "${var.stage}"
+//  asg_type = "default"
+//
+//  eks_cluster_name                       = "${module.eks-cluster.cluster_eks_name}"
+//  eks_cluster_endpoint                   = "${module.eks-cluster.cluster_eks_endpoint}"
+//  eks_cluster_certificate_authority_data = "${module.eks-cluster.cluster_certificate_authority_data}"
+//  eks_cluster_security_group_id          = "${module.eks-cluster.cluster_eks_sg_id}"
+//
+//  min_size      = 1
+//  max_size      = 4
+//  instance_type = "t3.small"
+//
+//  # pick the right image from here: https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html
+//  image_id = "ami-0abcb9f9190e867ab"
+//
+//  vpc_id     = "${module.network.vpc_id}"
+//  subnet_ids = "${split(",", module.network.public_subnet_ids)}"
+//}
+//
+//data "aws_iam_policy_document" "workers_default_iam_policy_extension" {
+//  statement {
+//    effect    = "Allow"
+//    actions   = ["s3:Get*"]
+//    resources = ["*"]
+//  }
+//}
+//
+//resource "aws_iam_role_policy" "workers_default_iam_role_extension" {
+//  policy = "${data.aws_iam_policy_document.workers_default_iam_policy_extension.json}"
+//  role   = "${module.eks-workers-default.worker_role_id}"
+//}
